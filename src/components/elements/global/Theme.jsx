50bf09b628b5ed43b5d3550280b6b0f0e457f1f1
abbas <abbas77jsalahi@gmail.com>
2022-11-01 13:55:38 +0330

    add palete

--- src/components/elements/global/Theme.jsx
+++ src/components/elements/global/Theme.jsx
@@ -2,10 +2,19 @@ import { createTheme } from "@mui/material";
 
 const Theme=createTheme({
     palette:{
+        
         golding:{
             main:"#e5c233",
             contrastText:"#000"
-        }
+        },
+        dork:{
+            main:"rgb(15, 15, 15)",
+            contrastText:"#fff"
+        },
+        light:{
+            main:"#fff",
+            contrastText:"#000"
+        },
     },
     
 })
