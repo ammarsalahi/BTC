327cdddeb2a91ec5ce8415167c476d6081d4b22c
awrsh <arashzarandi@gmail.com>
2022-11-15 23:52:28 +0330

    fix landing

--- src/components/elements/landing/TrideAny.jsx
+++ src/components/elements/landing/TrideAny.jsx
@@ -7,28 +7,28 @@ import AndroidIcon from '@mui/icons-material/Android';
 import LocalMallIcon from '@mui/icons-material/LocalMall';
 function TrideAny() {
     return (
-        <Box className="row">
-            <Box className="col-md-5">
+        <Box className="row" sx={{py: 15}}>
+            <Box className="col-lg-5 col-12">
                 <Box className="pictrade">
                     <img src={trade} width="100%" style={{ objectFit: "cover" }} alt="" />
                 </Box>
             </Box>
-            <Box className="col-md-4" sx={{ml: 20}}>
+            <Box className="col-lg-4 col-12" sx={{ml: 20}}>
                 <Typography variant="p" component="div" sx={{  fontSize: "40px", fontWeight: "bold", lineHeight: "3rem", pb: 4 }}>
                     Trade Anytime, Anywhere
                 </Typography>
                 <Box className='socialicon d-flex flex-wrap'>
-                    <Box className="col-6">
-                       <AppleIcon sx={{fontSize: "2.5rem"}}/>
+                    <Box className="col-6 py-3 d-flex flex-column">
+                       <AppleIcon sx={{fontSize: "3.5rem"}}/>
                     </Box>
-                    <Box className="col-6">
-                        <GoogleIcon sx={{fontSize: "2.5rem"}}/>
+                    <Box className="col-6 py-3">
+                        <GoogleIcon sx={{fontSize: "3.5rem"}}/>
                     </Box>
-                    <Box className="col-6">
-                        <AndroidIcon sx={{fontSize: "2.5rem"}}/>
+                    <Box className="col-6 py-3">
+                        <AndroidIcon sx={{fontSize: "3.5rem"}}/>
                     </Box>
-                    <Box className="col-6">
-                       <LocalMallIcon sx={{fontSize: "2.5rem"}}/>
+                    <Box className="col-6 py-3">
+                       <LocalMallIcon sx={{fontSize: "3.5rem"}}/>
                     </Box>
                 </Box>
             </Box>
